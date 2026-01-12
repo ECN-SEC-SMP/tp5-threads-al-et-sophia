4cae82b578b3b7ef4211ff5dbdaa38855f31a14a
AL <stephantandrelouis@gmail.com>
2026-01-12 18:03:43 +0100

    Question 2.2 OK
    Co-authored-by: sophivlk <sophivlk@users.noreply.github.com>

--- TPThreadsTrains/Controleur.hpp
+++ TPThreadsTrains/Controleur.hpp
@@ -1,38 +1,57 @@
 #ifndef CONTROLEUR_HPP
 #define CONTROLEUR_HPP
 
-#include <thread>             
+#include <thread>
 #include <iostream>
 #include <mutex>
 #include <condition_variable>
 
-using namespace std; 
+using namespace std;
 
-class Controleur {
-  public:
-    Controleur (int valeur_initiale) : 
-    val(valeur_initiale)
+class Controleur
+{
+public:
+  Controleur(int valeur_initiale) : val(valeur_initiale)
+  {
+    cout << "valeur initiale du sémaphore = " << val << endl;
+  }
+
+  // Entrée en B, si val <=0, return true (autorisation de passer)
+  inline bool controlinEnB(int numero)
+  {
+    if (val <= 0 && val >= -2) // Maximum de trains autorisés dans ce sens = 2
     {
-    }
-    
-    bool controlinEnB(int numero) { 
-        return true;
-    }  
-
-    bool controlinEnA(int numero) { 
-        return true;
-    }  
-
-    bool controloutEnB(int numero){ 
-      return true ;
-    }
-
-    bool controloutEnA(int numero){ 
+      val--;
       return true;
     }
+    return false;
+  }
 
-  private:
-    int val;
+  // Entrée en A, si val >=0, return true (autorisation de passer)
+  inline bool controlinEnA(int numero)
+  {
+    if (val >= 0 && val <= 2) // Maximum de trains autorisés dans ce sens = 2
+    {
+      val++;
+      return true;
+    }
+    return false;
+  }
+
+  inline bool controloutEnB(int numero)
+  {
+    val--; // "Incrément" du sens vers B
+    return true;
+  }
+
+  inline bool controloutEnA(int numero)
+  {
+    val++; // "Incrément" du sens vers A
+    return true;
+  }
+
+private:
+  int val = 0; // Direction du train sur la voie
 };
 
-#endif //CONTROLEUR_HPP
+#endif // CONTROLEUR_HPP
